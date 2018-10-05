ce9146ef8f79a53fe6fc8786f6a857c4dc1b07a2
gireinvent <girishkattasiddes@gmail.com>
2018-10-05 18:43:02 -0500

    commit

--- Deserialize.cs
+++ Deserialize.cs
@@ -10,18 +10,23 @@ namespace UFTRepo
     {
          static void Main(string[] args)
          {
-             //string path = @"C:\UFTRepo\Repository_Salesforce.xml";
+             string path = @"C:\UFTRepo\Repository_Salesforce.xml";
 
                 //XmlSerializer serializer = new XmlSerializer(typeof(ObjectRepository));
                 string text = System.IO.File.ReadAllText(@"C:\UFTRepo\Repository_Salesforce.xml");
+                XmlReader reader =  XmlReader.Create(path);
+                XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(ObjectRepository));
+                ObjectRepository response =  (ObjectRepository)serializer.Deserialize(reader);
             //StreamReader reader = new StreamReader(path);
 
-                using(TextReader sr = new StringReader(text))
-                {
-                        var serializer = new System.Xml.Serialization.XmlSerializer(typeof(ObjectRepository));
-                        ObjectRepository response =  (ObjectRepository)serializer.Deserialize(sr);
-                        //Console.WriteLine(response.bar);
-                }
+                // using(TextReader sr = new StringReader(text))
+                // {
+                //     //XmlSerializer serializer = new XmlSerializer(typeof(Person));
+                //     //Person person = (Person)serializer.Deserialize(reader);
+                //         XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(ObjectRepository));
+                //         ObjectRepository response =  (ObjectRepository)serializer.Deserialize(sr);
+                //         //Console.WriteLine(response.bar);
+                // }
          
          
           //  string strxml = reader.ReadToEndAsync();
